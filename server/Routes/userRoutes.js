384714859bc3d73621d15d6eeeedbc5de4640d53
Demi <127941175+demiladenotjulius@users.noreply.github.com>
2024-02-20 09:21:44 +0100

    the registraion

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,1 +1,14 @@
 // User Routes
+ const express = require('express')
+ const router = express.Router()
+
+ const registerUser = require('../Controllers/userController')
+
+router.post ('/register', registerUser)
+
+
+
+
+
+
+module.exports = router

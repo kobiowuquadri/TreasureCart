0c24553e55a48ee91d1097ef5d88dfbe85d85ac5
kobiowuquadri <kobiowuq@gmail.com>
2024-03-04 16:18:27 -0600

    add admin login logic

--- server/Controllers/adminController.js
+++ server/Controllers/adminController.js
@@ -25,4 +25,44 @@ const adminRegister = async (req, res) => {
      
 }
 
-module.exports = adminRegister
+const adminLogin = async (req, res) => {
+    try{
+    const {email, password}= req.body
+    const admin = await Admin.findOne({email})
+
+    if (!admin) {
+        return res.status(404).json({ success: false, message: 'Admin not found' })
+    }
+    const checkAdminPassword = await bcrypt.compare(password, checkAdminUser.password)
+    if (!checkAdminPassword) {
+      return res
+        .status(401)
+        .json({ success: false, message: 'Invalid Password' })
+    }
+
+    jwt.sign(
+        { id: admin._id },
+        process.env.SECRET,
+        { expiresIn: '1hr' },
+        async (err, token) => {
+          if (err) {
+            throw err
+          }
+          res.cookie('userId', admin._id, { maxAge: period, httpOnly: true })
+          res.status(200).json({
+            success: true,
+            message: 'Admin Login Successfully',
+            admin,
+            token
+          })
+        }
+      )
+
+} catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: 'Internal Server Error' });
+}
+
+}
+
+module.exports = {adminLogin, adminRegister}
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const userRouter = express.Router()
 const {registerUser, uploadImage, loginUser} = require('../Controllers/userController')
-const adminRegister = require('../Controllers/adminController')
+const {adminLogin, adminRegister} = require('../Controllers/adminController')
 const upload = require('../Middlewares/uploadImage')
 
-userRouter.post('/api/upload', upload.single('file'), uploadImage)
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
+userRouter.post('/api/upload', upload.single('file'), uploadImage)
 userRouter.post('/adminregister', adminRegister)
+userRouter.post('/adminlogin', adminLogin)
 
 module.exports = userRouter

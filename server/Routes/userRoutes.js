8f9aba25108c7648e08553ce5353559277ec4e71
Demi <127941175+demiladenotjulius@users.noreply.github.com>
2024-03-04 19:56:32 +0100

    update branch

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,7 +1,10 @@
 // User Routes
 const express = require('express')
 const userRouter = express.Router()
-const {registerUser, updateUser, allUsers , loginUser} = require('../Controllers/userController')
+const {registerUser, updateUser, allUsers } = require('../Controllers/userController')
 userRouter.post('/register', registerUser)
+userRouter.put ('/update/:id', updateUser)
+userRouter.get ('/allusers', allUsers)
+
 
 module.exports = userRouter

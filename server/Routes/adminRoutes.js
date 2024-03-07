8e585c5fcd7a22e4dae8ef4d654359abc0d37aa4
Quadri Kobiowu <97671765+kobiowuquadri@users.noreply.github.com>
2024-03-07 13:50:31 -0600

    Revert "Revert "subadmin: model, register, login""

--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const adminRouter = express.Router()
-const {adminLogin, adminRegister} = require('../Controllers/adminController')
+const {adminLogin, adminRegister, subadminRegister} = require('../Controllers/adminController')
+const subadminLogin = require('../Controllers/subadminController')
 
 
 adminRouter.post('/admin-register', adminRegister)
+adminRouter.post('/subadmin-register', subadminRegister)
 adminRouter.post('/admin-login', adminLogin)
+adminRouter.post('/subadmin-login', subadminLogin)
 
 
 module.exports = adminRouter

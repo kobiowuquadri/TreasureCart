684bee6b0799f21f7824580233f143a99267c467
Engr_Ifex <ayobamiadesoji2@gmail.com>
2024-03-07 09:07:45 -0800

    subadmin: model, register, login

---
+++ server/Controllers/subadminController.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const subAdmin = require('../Models/subadminModel')
+
+
+
+const period= 1000 * 60 * 60 * 24 * 3
+const subadminLogin = async (req, res) => {
+    
+    try{
+        const { adminId, password} = req.body
+        const subAdmin = await subAdmin.findOne({adminId})
+
+        if (!subAdmin) {
+            return res.status(404).json({ success: false, message: 'Admin not found' })
+        }
+        const isPassword = await bcrypt.compare(password, subAdmin.password)
+        if (!isPassword) {
+          return res
+            .status(401)
+            .json({ success: false, message: 'Invalid Password' })
+        }
+
+        jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: '1d'}, async (err, token) => {
+            if(err) {
+                throw err;
+            }
+            res.cookie('userid', user._id, {maxAge: period, httpOnly: true})
+            res.status(200).json({ success: true, message: 'login successful', subAdmin, token })
+        })
+    }
+    catch(err){
+        console.log(err.message)
+    }
+}
+
+module.exports = subadminLogin

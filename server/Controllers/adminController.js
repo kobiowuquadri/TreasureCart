b4822643ffb2a3650d677424c31d05c6244da8fa
olamilekan <olanrewajuolamilekan39@gmail.com>
2024-03-10 10:52:57 +0100

    allsubadmin and alluser added

--- server/Controllers/adminController.js
+++ server/Controllers/adminController.js
@@ -1,113 +1,145 @@
-const Admin = require('../Models/adminModel')
-const subAdmin = require('../Models/subadminModel')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const { User, passwordValidator } = require('../Models/userModels')
+const Admin = require("../Models/adminModel");
+const subAdmin = require("../Models/subadminModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, passwordValidator } = require("../Models/userModels");
 
-const period = 1000 * 60 * 60 * 24 * 3
-
+const period = 1000 * 60 * 60 * 24 * 3;
 
 const adminRegister = async (req, res) => {
-    try{
-        const { email, password, username} = req.body
-        const existingAdmin = await Admin.findOne({ email})
-        if(existingAdmin){
-            return res.status(404).json({ success: false,  message: 'Admin already Registered' })
-        }
-        const hashPassword = await bcrypt.hash(password, 10)
-        const newAdmin = Admin({
-            email,
-            password : hashPassword,
-            username
-        })
-        const savedAdmin = await newAdmin.save()
-        res.status(200).json({ success: true, message: 'Admin created', savedAdmin })
+  try {
+    const { email, password, username } = req.body;
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Admin already Registered" });
     }
-    catch(err){
-        console.log(err.message)
-    }
-     
-}
-
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newAdmin = Admin({
+      email,
+      password: hashPassword,
+      username,
+    });
+    const savedAdmin = await newAdmin.save();
+    res
+      .status(200)
+      .json({ success: true, message: "Admin created", savedAdmin });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 const subadminRegister = async (req, res) => {
-  try{
-      const { email, password, username} = req.body
-      const existingsubAdmin = await subAdmin.findOne({email})
-      if(existingsubAdmin){
-          return res.status(404).json({ success: false,  message: 'subAdmin already Registered' })
-      }
-      const hashPassword = await bcrypt.hash(password, 10)
-      const newsubAdmin = subAdmin({
-          email,
-          password : hashPassword,
-          username
-      })
-      const savedsubAdmin = await newsubAdmin.save()
-      res.status(200).json({ success: true, message: 'subAdmin created', savedsubAdmin })
+  try {
+    const { email, password, username } = req.body;
+    const existingsubAdmin = await subAdmin.findOne({ email });
+    if (existingsubAdmin) {
+      return res
+        .status(404)
+        .json({ success: false, message: "subAdmin already Registered" });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newsubAdmin = subAdmin({
+      email,
+      password: hashPassword,
+      username,
+    });
+    const savedsubAdmin = await newsubAdmin.save();
+    res
+      .status(200)
+      .json({ success: true, message: "subAdmin created", savedsubAdmin });
+  } catch (err) {
+    console.log(err.message);
   }
-  catch(err){
-      console.log(err.message)
-  }
-   
-}
-
+};
 
 const adminLogin = async (req, res) => {
-    try{
-    const {email, password}= req.body
-    const isAdmin = await Admin.findOne({email})
+  try {
+    const { email, password } = req.body;
+    const isAdmin = await Admin.findOne({ email });
 
     if (!isAdmin) {
-        return res.status(404).json({ success: false, message: 'Admin not found' })
+      return res
+        .status(404)
+        .json({ success: false, message: "Admin not found" });
     }
-    const checkAdminPassword = await bcrypt.compare(password, isAdmin.password)
+    const checkAdminPassword = await bcrypt.compare(password, isAdmin.password);
     if (!checkAdminPassword) {
       return res
         .status(401)
-        .json({ success: false, message: 'Invalid Password' })
+        .json({ success: false, message: "Invalid Password" });
     }
 
     jwt.sign(
-        { id: isAdmin._id },
-        process.env.SECRET,
-        { expiresIn: '1hr' },
-        async (err, token) => {
-          if (err) {
-            throw err
-          }
-          res.cookie('userId', isAdmin._id, { maxAge: period, httpOnly: true })
-          res.status(200).json({
-            success: true,
-            message: 'Admin Login Successfully',
-            isAdmin,
-            token
-          })
+      { id: isAdmin._id },
+      process.env.SECRET,
+      { expiresIn: "1hr" },
+      async (err, token) => {
+        if (err) {
+          throw err;
         }
-      )
-
-} catch (error) {
+        res.cookie("userId", isAdmin._id, { maxAge: period, httpOnly: true });
+        res.status(200).json({
+          success: true,
+          message: "Admin Login Successfully",
+          isAdmin,
+          token,
+        });
+      }
+    );
+  } catch (error) {
     console.error(error);
-    return res.status(500).json({ success: false, message: 'Internal Server Error' });
-}
-
-}
-
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 const updateProfileByAdmin = async (req, res) => {
-    try {
-      const { userId } = req.params;
-  
-      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
-        new: true
-      })
+  try {
+    const { userId } = req.params;
 
-      res.status(200).json({ success: true, message: 'User profile updated', updatedUser });
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).json({ success: false, message: 'Internal Server Error' });
-    }
-  };
-  
-  
-module.exports = {adminLogin, adminRegister, subadminRegister, updateProfileByAdmin}
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "User profile updated", updatedUser });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+const allSubadmin = async (req, res) => {
+  try {
+    const users = await subAdmin.find();
+    res
+      .status(202)
+      .json({ success: true, message: "View all subadmin successful", users });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const allUser = async (req, res) => {
+  try {
+    const users = await User.find();
+    res
+      .status(202)
+      .json({ success: true, message: "View all users Successful", users });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports = {
+  adminLogin,
+  adminRegister,
+  subadminRegister,
+  updateProfileByAdmin,
+  allUser,
+  allSubadmin,
+};
--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const adminRouter = express.Router()
-const {adminLogin, adminRegister, subadminRegister, updateProfileByAdmin} = require('../Controllers/adminController')
-const subadminLogin = require('../Controllers/subadminController')
+const {adminLogin, adminRegister, subadminRegister, updateProfileByAdmin, allUser, allSubadmin} = require('../Controllers/adminController')
+const {subadminLogin} = require('../Controllers/subadminController')
 const adminController = require('../Controllers/adminController')
 const verifyAdmin = require('../Middlewares/verifyAdmin')
 
@@ -10,6 +10,8 @@ adminRouter.post('/subadmin-register', subadminRegister)
 adminRouter.post('/admin-login', adminLogin)
 adminRouter.post('/subadmin-login', subadminLogin)
 adminRouter.put('/admin/update-user/:userId', verifyAdmin, updateProfileByAdmin);
+adminRouter.get('/allSubAdim', verifyAdmin, allSubadmin)
+adminRouter.get('/allUser', verifyAdmin, allUser)
 
 
 module.exports = adminRouter

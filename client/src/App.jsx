6787dfe078f9a417ed305a5e4a81b936d266abcf
rabi-codes <rabeehadetomi@gmail.com>
2024-04-12 00:20:52 +0100

    contact page included

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Login from "./Pages/User/LogIn/LogIn";
-import Register from './Pages/User/Register/Register';
-import {Route, Routes } from "react-router"
+import Register from "./Pages/User/Register/Register";
+import { Route, Routes } from "react-router";
 import Home from "./Pages/Home/Home";
-import './App.css'
+import "./App.css";
+import ContactUs from "./Components/ContactUs/ContactUs";
 
 function App() {
   return (
@@ -13,11 +14,11 @@ function App() {
           <Route index element={<Home />}></Route>
           <Route path="/Login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/contact-us" element={<ContactUs />}></Route>
         </Routes>
       </div>
     </>
   );
-
 }
 
 export default App;

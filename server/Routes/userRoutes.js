28855d9c5006fddd926e87252548af2ab5654e2e
kobiowuquadri <kobiowuq@gmail.com>
2024-05-12 09:59:48 -0500

    fix: merge conflicts

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,23 +1,3 @@
-const express = require('express');
-const userRouter = express.Router();
-const { registerUser, updateUser, loginUser } = require('../Controllers/userController');
-const verifyUser = require('../Middlewares/verifyUser');
-
-
-userRouter.post('/user-register', registerUser);
-userRouter.post('/user-login', loginUser);
-userRouter.put('/user-update/:id', verifyUser, updateUser);
-
-
-
-
-
-module.exports = userRouter;
-
-
-
-
-
 // User Routes
 const express = require('express')
 const userRouter = express.Router()
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-require('dotenv').config()
 const connectToDB = require('./Database/db')
 const userRouter = require('./Routes/userRoutes')
 const adminRouter = require('./Routes/adminRoutes')
@@ -8,31 +8,7 @@ const cookieParser = require ('cookie-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
-
-const port = process.env.PORT
-
-
-
-app.listen(port, () => {
-   console.log(`Server running on port ${port}`)
-})
-
-
-const allowedOrigins = [
-   'http://localhost:5173', // Add your local development environment
-   'https://treasure-cart.vercel.app' // Add your production frontend URL
- ];
- 
-
-app.use(
-   cors({
-     origin: allowedOrigins,
-     credentials: true,
-   })
- );
- app.use('/api/users', userRouter); // Example endpoint
-
-
+const port = process.env.PORT 
 
 app.use(cors({
    origin: ['http://localhost:5173', 'https://backend-treasure-cart.onrender.com'],
@@ -47,12 +23,6 @@ app.use(userRouter)
 app.use(adminRouter)
 app.use(cookieParser())
 
-
-
-
-app.get('/', (req, res) => {
-   res.send('Backend Working Successfully.')
-})
 
 // HOME
 app.get('/', (req, res) => {
